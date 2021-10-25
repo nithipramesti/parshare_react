09a2f89a4e9bfc7d6b8e7b1fd8a6dc75c79ccf37
thecodesdev <ilhamaulana24@gmail.com>
2021-10-25 23:15:29 +0700

    PAR-20: Add graph for each product

--- src/pages/admin/Stats.jsx
+++ src/pages/admin/Stats.jsx
@@ -8,11 +8,15 @@ import { API_URL } from '../../data/API';
 function Stats(){
   const authReducer = useSelector((state) => state.authReducer);
   const [ parcelList, setParcelList ] = useState([]);
+  const [ productList, setProductList ] = useState([]);
   const [ selectParcel, setSelectParcel ] = useState(1);
+  const [ selectProduct, setSelectProduct ] = useState(1);
   const [ incomeSeries, setIncomeSeries ] = useState([]);
   const [ incomeOptions, setIncomeOptions ] = useState({});
   const [ parcelSeries, setParcelSeries ] = useState([]);
   const [ parcelOptions, setParcelOptions ] = useState({});
+  const [ productSeries, setProductSeries ] = useState([]);
+  const [ productOptions, setProductOptions ] = useState({});
   const [ selectPeriod, setSelectPeriod ] = useState(30);
   const [ availablePeriods, setAvailablePeriods ] = useState([7, 14, 30]);
 
@@ -30,6 +34,9 @@ function Stats(){
       let totalPrice = (obj).map(res => {
         return res.totalPrice
       });
+      let totalCapital = (obj).map(res => {
+        return res.totalPrice - res.income
+      });
       let date = (obj).map(res => {
         return res.date
       })
@@ -37,6 +44,10 @@ function Stats(){
         {
           name: "Income",
           data: income
+        },
+        {
+          name: "Capital",
+          data: totalCapital
         },
         {
           name: "Total Price",
@@ -98,6 +109,9 @@ function Stats(){
       let totalMargin = (obj).map(res => {
         return res.totalMargin
       });
+      let totalCapital = (obj).map(res => {
+        return res.totalPrice - res.totalMargin
+      });
       let date = (obj).map(res => {
         return res.date
       })
@@ -107,12 +121,16 @@ function Stats(){
           data: totalSold
         },
         {
-          name: "Total Price",
-          data: totalPrice
-        },
-        {
           name: "Margin",
           data: totalMargin
+        },
+        {
+          name: "Capital",
+          data: totalCapital
+        },
+        {
+          name: "Total Price",
+          data: totalPrice
         }
       ])
       setParcelOptions({
@@ -157,10 +175,77 @@ function Stats(){
       alert("Error: " + err.message);
     })
   }
+  const fetchProductData = (id, period) => {
+    Axios.get(`${API_URL}/products/sold?id=${id}&period=${period}`, {
+      headers: {
+        'Authorization': `Bearer ${localStorage.getItem("token_parshare")}`
+      }
+    })
+    .then(res => {
+      let obj = res.data.data.reverse();
+      let totalSold = (obj).map(res => {
+        return res.total
+      });
+      let date = (obj).map(res => {
+        return res.date
+      })
+      setProductSeries([
+        {
+          name: "Sold",
+          data: totalSold
+        }
+      ])
+      setProductOptions({
+        chart: {
+          id: "area",
+          toolbar: {
+            show: false
+          },
+          tools: {
+            download: false,
+            selection: false,
+            zoom: false,
+            zoomin: false,
+            zoomout: false,
+            pan: false,
+            reset: false | '<img src="/static/icons/reset.png" width="20">',
+            customIcons: []
+          },
+          zoom: {
+            enabled: false
+          }
+        },
+        xaxis: {
+          type: "datetime",
+          categories: date
+        },
+        tooltip: {
+          followCursor: true,
+          y: {
+            formatter: function(value, {seriesIndex}) {
+              return value.toLocaleString();
+            }
+          }
+        }
+      });
+    })
+    .catch(err => {
+      alert("Error: " + err.message);
+    })
+  }
   const fetchParcelList = () => {
     Axios.get(`${API_URL}/parcels/list`)
     .then(res => {
       setParcelList(res.data.data)
+    })
+    .catch(err => {
+      alert("Error: " + err.message);
+    })
+  }
+  const fetchProductList = () => {
+    Axios.get(`${API_URL}/products/list`)
+    .then(res => {
+      setProductList(res.data.data)
     })
     .catch(err => {
       alert("Error: " + err.message);
@@ -171,9 +256,15 @@ function Stats(){
     setSelectParcel(value);
     fetchParcelData(value, selectPeriod);
   }
+  const changeProduct = (e) => {
+    const value = e.target.value;
+    setSelectProduct(value);
+    fetchProductData(value, selectPeriod);
+  }
   const selectPeriodHandler = (period) => {
     setSelectPeriod(period);
     fetchParcelData(selectParcel, period);
+    fetchProductData(selectProduct, period);
     fetchIncome(period);
   }
 
@@ -184,6 +275,8 @@ function Stats(){
       fetchIncome();
       fetchParcelData(1);
       fetchParcelList();
+      fetchProductData(1);
+      fetchProductList();
     }
   }, []);
   
@@ -225,6 +318,22 @@ function Stats(){
           </select>
           <h5 style={{display: 'inline-block', marginRight: "10px"}}>sold in {selectPeriod} days</h5>
           <Chart options={parcelOptions} type="area" series={parcelSeries} width="100%" height="300px" />
+        </div>
+        <div className="border rounded mt-4">
+          <h5 style={{marginTop: "10px", marginLeft: "10px", display: "inline-block"}}>Total product</h5>
+          <select style={{display: 'inline-block', width: "initial", marginLeft: "10px", marginRight: "10px"}} class="form-select" aria-label="Default select example" name="parcel" onChange={changeProduct}>
+            {
+              productList.map(parcel => {
+                if(selectProduct === parcel.id_product){
+                  return <option value={parcel.id_product} selected>{parcel.product_name}</option>
+                }else{
+                  return <option value={parcel.id_product}>{parcel.product_name}</option>
+                }
+              })
+            }
+          </select>
+          <h5 style={{display: 'inline-block', marginRight: "10px"}}>sold in {selectPeriod} days</h5>
+          <Chart options={productOptions} type="area" series={productSeries} width="100%" height="300px" />
         </div>
       </div>
     )
